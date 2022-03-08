cdd6e487249086571b475e3166132dc06925cba6
farohan <68710283+farohan@users.noreply.github.com>
2022-03-07 20:11:03 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//Prevents form from refreshing the page
+
 const movieForm = document.getElementById('movie-searcher');
 const submitButton = document.getElementById('submit-btn');
 
@@ -7,10 +9,51 @@ function formHandler(event) {
 
 movieForm.addEventListener('submit', formHandler);
 
-const movieQuery = document.getElementById('movie-name').value;
-const apiKey = 'dc643bbb';
-let url = `http://www.omdbapi.com/?apikey=${apiKey}/?t=star+wars&plot=full`;
+//Calling the API through Fetch
 
-console.log(url);
+submitButton.onclick = async function fetchMovie() {
 
-submitButton.onclick = ''; //put fetch api here
+    //Getting the movie name from searchbar
+
+    let movieQuery = document.getElementById('movie-name').value;
+    movieQuery = movieQuery.replaceAll(' ', '');
+
+    //Setting the API url
+
+    const apiKey = 'dc643bbb';
+    let url = `http://www.omdbapi.com/?apikey=${apiKey}&t=${movieQuery}&plot=full`;
+
+    //Fetching actual data from OMDb
+
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+
+    //Setting Up Image Poster
+
+    const movieImg = document.getElementById('movie-img');
+
+    movieImg.src = data.Poster;
+    movieImg.alt = `${data.Title}'s Poster`;
+
+    //And the title
+
+    const movieTitle = document.getElementById('movie-title');
+    movieTitle.innerHTML = data.Title;
+
+    //Finally, filling it with movie data
+
+    const movieData = document.getElementById('movie-data');
+    movieData.innerHTML = 
+    `
+    <strong>Type:</strong> ${data.Type} <br>
+    <strong>Genre:</strong> ${data.Genre} <br>
+    <strong>Plot:</strong> ${data.Plot} <br>
+    <strong>Actors:</strong> ${data.Actors} <br>
+    <strong>Runtime:</strong> ${data.Runtime} <br>
+    <strong>Rated:</strong> ${data.Rated} <br>
+    <strong>Release:</strong> ${data.Released} <br>
+    <strong>Box Office:</strong> ${data.BoxOffice} <br>
+    <strong>Country:</strong> ${data.Country} 
+    `;
+}
